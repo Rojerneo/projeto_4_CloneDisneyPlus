735123eca16b30806c8f442ed2eed8c92fa17cb4
Rogerio Oliveira <rogerioantoniooliveira@gmail.com>
2023-10-17 14:25:22 -0300

    Criação da sessão FAQ.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]')
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(button) {
@@ -13,7 +14,17 @@ document.addEventListener('DOMContentLoaded', function() {
             button.target.classList.add('shows__tabs__button--is-active')
         })
     }
+
+    for (let i = 0; i < questions.length; i++)
+        questions[i].addEventListener('click', abreFechaResposta);
 })
+
+function abreFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeButtonActive() {
     const buttons = document.querySelectorAll('[data-tab-button]');
